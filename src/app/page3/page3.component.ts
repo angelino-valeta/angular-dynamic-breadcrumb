32210befb3d908787ee499d89f2e659890a9db28
Angelino Valeta <angelinovaleta@gmail.com>
2022-08-05 15:47:50 +0100

    implement method update breadcrumb

--- src/app/page3/page3.component.ts
+++ src/app/page3/page3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgDynamicBreadcrumbService } from 'ng-dynamic-breadcrumb';
 
 @Component({
   selector: 'app-page3',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page3Component implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ngDynamicBreadcrumbService: NgDynamicBreadcrumbService,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    const breadcrumb = { customText: 'Custom Text ' };
+    this.ngDynamicBreadcrumbService.updateBreadcrumbLabels(breadcrumb);
+  }
+
+  updateBreadcrumb(): void {
+    const breadcrumbs = [
+      {
+        label: 'page {{pageOneID}}',
+        url: '/page1/:pageOneID'
+      },
+      {
+        label: 'page {{pageTwoID}}',
+        url: 'page1/:pageOneID/page2/:pageTwoID'
+      },
+      {
+        label: 'page {{pageThreeID}}',
+        url: 'page1/:pageOneID/page2/:pageTwoID/page3/:pageThreeID'
+      },
+      {
+        label: 'Update Breadcrumb',
+        url: ''
+      }
+    ]
+    this.ngDynamicBreadcrumbService.updateBreadcrumb(breadcrumbs)
   }
 
 }
