50295d3883e983f8318279e3e2a35d9d9cb38970
Angelino Valeta <angelinovaleta@gmail.com>
2022-08-05 14:38:37 +0100

    add page 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageComponent } from './page/page.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'page1/:pageOneId',
+    component: PageComponent,
+    data: {
+      title: 'page1',
+      breadcrumb: [
+        {
+          label: 'Page'
+        }
+      ]
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    Page2Component,
+    Page3Component
   ],
   imports: [
     BrowserModule,
